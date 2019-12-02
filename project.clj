cab5fb6a36883c42fb479e9708971f892d4a464c
Marc-Olivier Fiset <marco.fiset@gmail.com>
2019-12-01 20:47:31 -0500

    remove unnecessary folders and update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject advent-of-code-2019 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject advent-of-code-2019 "0.1.0"
+  :description "My solutions to Advent of Code 2019 using Clojure"
+  :url "https://github.com/marco-fiset/advent-of-code-2019"
   :dependencies [[org.clojure/clojure "1.8.0"]])
